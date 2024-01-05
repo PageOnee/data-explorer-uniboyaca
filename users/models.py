1df14bded8226f050983b09788f9fbfc6b4a522b
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-05 10:39:06 -0500

    Agrega el Modelo de Users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Todo : Clase Usuario Bd 
+class Users(models.Model):
+    
+    userName = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    
+    # ** Configuro vista del Administrador para el Usuario
+    def __str__(self):
+        return self.email
