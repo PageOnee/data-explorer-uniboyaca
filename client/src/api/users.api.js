60f5882c98ee54f6601bbc0bcb5f1eae121297f1
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-06 08:19:57 -0500

    Agrega la funcion de Traer Usuarios

--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -4,5 +4,8 @@ const userApi = axios.create({
   baseURL: "http://localhost:8000/data-explorer/api/v1/users/",
 });
 
+// ** Traer los usuarios
+export const getAllUser = () => userApi.get("/");
+
 // ** Guardar usuario registrado
 export const createUser = (user) => userApi.post("/", user);
