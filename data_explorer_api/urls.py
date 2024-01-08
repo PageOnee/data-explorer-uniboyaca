5d5b22817f9c291567a1f21501232910c2f62704
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-08 15:58:02 -0500

    Modifica el archivo de Django

--- data_explorer_api/urls.py
+++ data_explorer_api/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
 
+
 ### Todo : Configuracion de las rutas generales
 urlpatterns = [
     path('data-explorer/admin/', admin.site.urls),
--- data_explorer_api/wsgi.py
+++ data_explorer_api/wsgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_explorer_api.settings')
 application = get_wsgi_application()
