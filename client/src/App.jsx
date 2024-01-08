0c68e4e77e3ac216bcd73484affa7bfcc76a92d8
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-08 17:29:44 -0500

    Da formato al archivo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import {WavyContainer} from "react-wavy-transitions";
-import {UserRegister} from './pages/auth/register/UserRegister'
-import {Login} from './pages/auth/login/Login'
-import {Home} from './pages/data/home/Home'
-import './styles/App.css'
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { WavyContainer } from "react-wavy-transitions";
+import { UserRegister } from "./pages/auth/register/UserRegister";
+import { Login } from "./pages/auth/login/Login";
+import { Home } from "./pages/data/home/Home";
+import "./styles/App.css";
 
 function App() {
   return (
     <BrowserRouter>
       <WavyContainer />
       <Routes>
-        <Route path="/" element={<Navigate to="/inicio"/>} />
+        <Route path="/" element={<Navigate to="/inicio" />} />
         <Route path="registro-usuario" element={<UserRegister />} />
         <Route path="inicio-sesion" element={<Login />} />
         <Route path="inicio" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
