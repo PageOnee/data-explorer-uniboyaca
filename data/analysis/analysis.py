1fd2e6f38bd8ba1f810c4e16c3b9ecd07e8e3e20
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-11 14:04:50 -0500

    Agrega la funcionalidad del analisis con un archivo predefinido y columnas predefinadas

--- data/analysis/analysis.py
+++ data/analysis/analysis.py
@@ -1,40 +1,43 @@
 import pandas as pd
 import os
+import json
 from .load_data import LoadData
 from .methods import Methods
-import json
 
+
+## Todo - Realiza la automatización del analisis de datos : 
 class Analysis:
     
     def analysis_data(self):
+                 
+        # ** Ruta del directorio de los archivos excel
+        directory = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(directory, 'resources/post/2020_2.xlsx')
         
-        #print('Anaysis : Inicio Analisis : ')
-        
+        # ** Cargar datos
         load_df = LoadData()
-        current_directory = os.path.dirname(os.path.abspath(__file__))
-        file_path = os.path.join(current_directory, 'resources/post/2020_2.xlsx')
-        #print(file_path)
         df = load_df.load_data(file_path)
         
+        # ** Elimina columnas excedentes
         columns_delete = ['Nombre','JustificacionTiempoIngresoPost','JustificacionRecomendacion','Comentarios','ServiciosBienestarPostgrado','ServiciosAdicionalesPostgrado']
         df_count = df.drop(columns_delete, axis=1)
         
-        # ** Contar Columnas
+        # ** Cuenta el numero de columnas
         num_columns = len(df_count.columns)
         print ('Numero de Columnas es : ', num_columns)
         
-        # **
+        # ** Realiza el conteo de
         df_data = Methods.count_data(df_count,'Sede')
+        print('DataFrame : ')
         print(df_data)
         
-        # ** Convetir
-        data_share =pd.Series(df_data)
+        # ** Convertir el DataFrame
+        data_dic = df_data.to_dict()
+        print('Diccionario : ')
+        print(data_dic)
         
-        # print('Series')
-        #  print(data_share)
-        data_dic = data_share.to_dict()
+        data_response = json.dumps({'sede': data_dic}, indent=2)
+        print('JSON : ')
+        print(data_response)
         
-        # print(data_dic)
-        data_json = json.dumps(data_dic, indent=2)
-        
-        return data_json
+        return data_response
