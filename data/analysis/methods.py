c184aba3501054e6f7d62d2b3b8149340cd7bc47
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-11 18:24:55 -0500

    Modifica el metodo de 'Conteo' - Establece un diccionario que guarda los conteos individuales

--- data/analysis/methods.py
+++ data/analysis/methods.py
@@ -2,10 +2,16 @@ import pandas as pd
 import unicodedata
 
 
-## Todo - Metodos utilizados para realizar el Analisis de Datos :
+## Todo : Metodos utilizados para realizar el Analisis de Datos
 class Methods:
     
     # ** Metodo de conteo de cantidades :
-    def count_data(df, column):
-        df_data = df[column].value_counts()
-        return df_data
+    @staticmethod
+    def count_data(df, columns):
+        data_dict = {}
+        
+        for column in columns:
+            df_data = df[column].value_counts().to_dict()
+            data_dict[column] = df_data
+        return data_dict
+    
