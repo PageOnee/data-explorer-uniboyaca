6a151e04bcbf69df91b247414c676ee1f59634b1
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-12 12:16:12 -0500

    Modifica los estilos del 'Header' - Agrega titulo del 'Header' variable

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+export const Header = ({ title }) => {
+  return (
+    <div className="header">
+      <div className="header__title-header">
+        <h2>{title}</h2>
+      </div>
+
+      <div className="header__user-container">
+        <img
+          src="../../../public/assets/images/predefined-user.jpg"
+          alt="Foto de Perfil del Usuario"
+        />
+        <p>Maicol Rojas</p>
+      </div>
+    </div>
+  );
+};
