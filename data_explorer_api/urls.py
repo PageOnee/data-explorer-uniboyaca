15b62876a496f409f7b472b5b5cfddce13e85f01
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-14 20:25:33 -0500

    Modifica el archivo - Modifica las urls generales de las Apis

--- data_explorer_api/urls.py
+++ data_explorer_api/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 ### Todo : Configuracion de las rutas generales
 urlpatterns = [
     path('data-explorer/admin/', admin.site.urls),
-    path('data-explorer/', include('users.urls'))
+    path('data-explorer/', include('users.urls')),
+    path('data-explorer/',include('data.urls'))
 ]
