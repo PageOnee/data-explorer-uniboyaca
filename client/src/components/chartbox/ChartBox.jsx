11c75c5d426ae48e479ebf2c48baf550df5824f9
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-16 18:13:41 -0500

    Modifica el componente de la Caja de las graficas

--- client/src/components/chartbox/ChartBox.jsx
+++ client/src/components/chartbox/ChartBox.jsx
@@ -1,24 +1,15 @@
-import {Bar, BarChart, ResponsiveContainer, Tooltip} from 'recharts'
+import { BarCharts } from "../barChart/BarCharts";
 import "./ChartBox.css";
 
 export const ChartBox = (props) => {
-  return <div className="chart-box">
-    <div className="barChartBox">
-      <h1>{props.title}</h1>
-      <div className="chart">
-        <ResponsiveContainer width="99%" height={150}>
-          <BarChart data={props.chartData}>
-            <Tooltip
-              contentStyle={{ background: "#2a3447", borderRadius: "5px" }}
-              labelStyle={{ display: "none" }}
-              cursor={{ fill: "none" }}
-            />
-            <Bar dataKey={props.dataKey} fill={props.color} />
-          </BarChart>
-        </ResponsiveContainer>
-      </div>
+  return (
+    <div className="chart-box">
+      <BarCharts
+        title={props.title}
+        color={props.color}
+        dataKey={props.dataKey}
+        chartData={props.chartData}
+      />
     </div>
-  </div>;
+  );
 };
-
-
