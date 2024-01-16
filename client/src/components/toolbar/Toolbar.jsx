74b3dacdb5eafa33ad03573e3b3c81eee49bde76
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-16 16:40:30 -0500

    Modifica los estilos del componente - Agrega los dropdown de cada filtro

--- client/src/components/toolbar/Toolbar.jsx
+++ client/src/components/toolbar/Toolbar.jsx
@@ -2,26 +2,25 @@ import { useState } from "react";
 import { Dropdown } from "../dropdown/Dropdown";
 import "./Toolbar.css";
 
+// * Componente barra de herramientas :
 export const Toolbar = ({ onSelect }) => {
   const [selected, setSelected] = useState("");
 
   return (
     <div className="toolbar">
-      <div className="toolbar__order-drop">
+      <div className="toolbar__dropdown">
         <p> Ordenar Por : </p>
+        <Dropdown selected={selected} setSelected={setSelected} onSelect={onSelect} />
       </div>
 
-      <div className="toolbar__semester-drop">
+      <div className="toolbar__dropdown">
         <p> Semestre : </p>
+        <Dropdown selected={selected} setSelected={setSelected} onSelect={onSelect} />
       </div>
 
-      <div className="toolbar__information-drop">
+      <div className="toolbar__dropdown">
         <p> Filtrar Informacion Por : </p>
-        <Dropdown
-          selected={selected}
-          setSelected={setSelected}
-          onSelect={onSelect}
-        />
+        <Dropdown selected={selected} setSelected={setSelected} onSelect={onSelect} />
       </div>
     </div>
   );
