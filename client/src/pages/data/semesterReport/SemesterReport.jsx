ebace3baf58146a4eaacb1a7bc580b5a819e0f00
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-16 10:41:45 -0500

    Modifica el componente - Agrega la funcionalidad de crear el numero de graficas de manera dinamica

--- client/src/pages/data/semesterReport/SemesterReport.jsx
+++ client/src/pages/data/semesterReport/SemesterReport.jsx
@@ -3,12 +3,14 @@ import { SideMenu } from "../../../components/sidemenu/SideMenu";
 import { Header } from "../../../components/header/Header";
 import { Toolbar } from "../../../components/toolbar/Toolbar";
 import { ChartBox } from "../../../components/chartbox/ChartBox";
-import { getAllData } from "../../../services/data.api";
+import { barChartBoxVisit } from "../../../services/data";
+import { getInformacionPersonal } from "../../../services/data.api";
 import "./SemesterReport.css";
 
 export const SemesterReport = () => {
   const [selectedOption, setSelectedOption] = useState("");
-  const [data, setData] = useState([]);
+  const [informacionPersonal, setInformacionPersonal] = useState(null);
+  const [numObjetos, setNumObjetos] = useState(0); // Agregar el estado para numObjetos
 
   const handleDropdownSelection = (selectedOption) => {
     console.log("Seleccionaste:", selectedOption);
@@ -16,14 +18,28 @@ export const SemesterReport = () => {
   };
 
   useEffect(() => {
-    async function loadData() {
-      const res = await getAllData();
-      setData(res.data)
-      console.log(res.data);
+    if (selectedOption === "Informacion Personal") {
+      // Realizar la solicitud de la API cuando selectedOption es "Informacion Personal"
+      getInformacionPersonal()
+        .then((response) => {
+          console.log("Informacion Personal:", response.data);
+          setInformacionPersonal(response.data);
 
+          // Obtener el número de "objetos" en la respuesta e imprimirlo en la consola
+          const cantObjetos = Object.keys(response.data).length;
+          setNumObjetos(cantObjetos); // Actualizar el estado de numObjetos
+          console.log("Número de 'objetos':", numObjetos);
+        })
+        .catch((error) => {
+          console.error("Error al obtener Informacion Personal:", error);
+        });
     }
-    loadData();
-  }, []);
+  }, [selectedOption]); // Se ejecutará cada vez que selectedOption cambie
+
+  const chartBoxes = [];
+  for (let i = 0; i < numObjetos; i++) {
+    chartBoxes.push(<ChartBox key={i} {...barChartBoxVisit} />);
+  }
 
   return (
     <div className="semester-report">
@@ -31,8 +47,10 @@ export const SemesterReport = () => {
       <div className="semester-report__content">
         <Header title={"Reporte Semestral"} />
         <Toolbar onSelect={handleDropdownSelection} />
-        {selectedOption === "Informacion Personal" && <ChartBox />}
+        <div className="content__charts-box">
+          {chartBoxes}
+        </div>
       </div>
     </div>
   );
-};
+}
