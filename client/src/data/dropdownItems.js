70b6592c74f4c7b244064cf56ec3c458e36f7324
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-17 21:47:54 -0500

    Agrega los items de los menus desplegables

---
+++ client/src/data/dropdownItems.js
@@ -0,0 +1,80 @@
+export const orderItems = [
+  {
+    name: "Alfabeticamente A-Z",
+  },
+  {
+    name: "Alfabeticamente Z-A",
+  },
+];
+
+export const levelItems = [
+  {
+    name: "Pregrado",
+  },
+  {
+    name: "Posgrado",
+  },
+];
+
+export const periodItems = [
+  {
+    name: "Semestral",
+  },
+  {
+    name: "Anual",
+  },
+  {
+    name: "General",
+  },
+];
+
+export const lapseItems = [
+  {
+    name: "2020-2",
+  },
+  {
+    name: "2021-1",
+  },
+  {
+    name: "2021-2",
+  },
+  {
+    name: "2022-1",
+  },
+  {
+    name: "2022-2",
+  },
+  {
+    name: "2023-1",
+  },
+];
+
+export const annualLapseItems = [
+  {
+    name: "2020",
+  },
+  {
+    name: "2021",
+  },
+  {
+    name: "2022",
+  },
+  {
+    name: "2023",
+  },
+];
+
+export const categoryItems = [
+  {
+    name: "Informacion Personal",
+  },
+  {
+    name: "Educacion Previa",
+  },
+  {
+    name: "Percepcion y Satisfaccion",
+  },
+  {
+    name: "Estilo de Vida y Preferencias",
+  },
+];
