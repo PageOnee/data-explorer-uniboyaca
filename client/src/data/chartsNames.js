a7fdfd8c21e77ae99a6164666972699c0aa37141
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-17 21:23:07 -0500

    Agrega el menu de Items para la categoria Informacion Personal

---
+++ client/src/data/chartsNames.js
@@ -0,0 +1,26 @@
+export const PersonalInformationItems = [
+  {
+    name: "Sede : ",
+  },
+  {
+    name: "Programa Academico : ",
+  },
+  {
+    name: "Doble Programa : ",
+  },
+  {
+    name: "Sede Doble Programa : ",
+  },
+  {
+    name: "Programa Doble Programa : ",
+  },
+  {
+    name: "Sexo : ",
+  },
+  {
+    name: "Convive Con : ",
+  },
+  {
+    name: "Personas A Cargo : ",
+  },
+];
