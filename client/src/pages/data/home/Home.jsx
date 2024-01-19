58465dd7803453a62f8ba2e146eb430ee0692d63
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-18 20:08:41 -0500

    Elimina la barra de herramientas, Elimina los contenedores de graficas

--- client/src/pages/data/home/Home.jsx
+++ client/src/pages/data/home/Home.jsx
@@ -1,26 +1,13 @@
 import { Header } from "../../../components/header/Header";
 import { SideMenu } from "../../../components/sidemenu/SideMenu";
-import { Toolbar } from "../../../components/toolbar/Toolbar";
-import { ChartBox } from "../../../components/chartbox/ChartBox";
-import { barChartBoxVisit } from "../../../services/data";
 import "./Home.css";
 
 export const Home = () => {
-  const cantidadCharts = 14; // Puedes cambiar este valor según tu necesidad
-
-  // Crear un array con la cantidad deseada de ChartBox
-  const chartBoxes = [];
-  for (let i = 0; i < cantidadCharts; i++) {
-    chartBoxes.push(<ChartBox key={i} {...barChartBoxVisit} />);
-  }
-
   return (
     <div className="home">
       <SideMenu />
-      <div className="home__columns">
+      <div className="home__content">
         <Header />
-        <Toolbar />
-        <div className="columns__container-charts">{chartBoxes}</div>
       </div>
     </div>
   );
