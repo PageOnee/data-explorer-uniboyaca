13692444a9b9cffa6e454e52b57df3591ca7d927
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-19 11:18:19 -0500

    Agrega el nombre de las graficas para las categorias de Informacion Personal, Estudios Previos y Percepcion y Satisfaccion

--- client/src/data/chartsNames.js
+++ client/src/data/chartsNames.js
@@ -1,4 +1,4 @@
-export const PersonalInformationItems = [
+export const personalInformationItems = [
   {
     name: "Sede : ",
   },
@@ -22,5 +22,71 @@ export const PersonalInformationItems = [
   },
   {
     name: "Personas A Cargo : ",
+  },
+];
+
+export const previusEducationItems = [
+  {
+    name: "Tipo de Jordana",
+  },
+  {
+    name: "Validacion Bachillerato",
+  },
+  {
+    name: "Institucion Bachillerato Bilingue",
+  },
+  {
+    name: "Inconclusion Estudios Previos",
+  },
+  {
+    name: "Nivel Idioma Ingles",
+  },
+  {
+    name: "Nivel Idioma Frances",
+  },
+  {
+    name: "Nivel Idioma Italiano",
+  },
+  {
+    name: "Nivel Idioma Aleman",
+  },
+];
+
+export const perceptionItems = [
+  {
+    name: "Percepcion Equipos y Medios Tecnologicos",
+  },
+  {
+    name: "Percepcion Calidad de Docentes",
+  },
+  {
+    name: "Percepcion Planes de Estudio",
+  },
+  {
+    name: "Percepcion Servicios de Usuario",
+  },
+  {
+    name: "Percepcion Procesos Pedagogicos",
+  },
+  {
+    name: "Percepcion del Sistema de Evaluacion",
+  },
+  {
+    name: "Percepcion Imagen General",
+  },
+  {
+    name: "Satisfaccion Chat en Linea",
+  },
+  {
+    name: "Satisfaccion Fomulario del Sitio Web",
+  },
+  {
+    name: "Satisfaccion Whatsapp",
+  },
+  {
+    name: "Satisfaccion Correo Electronico",
+  },
+  {
+    name: "Satisfaccion Punto de Atencion Presencial",
   },
 ];
