37b2ab68825ece12233942320fbae9eed3c078f7
Maicol Rojas <maidanlrojas@gmail.com>
2024-01-29 14:13:58 -0500

    Agrega los toast de validacion para Inicio de sesion y campos vacios

--- client/src/components/forms/login/LoginForm.jsx
+++ client/src/components/forms/login/LoginForm.jsx
@@ -1,17 +1,13 @@
 import { WavyLink } from "react-wavy-transitions";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 import { getAllUser } from "../../../services/users.api";
 import "./LoginForm.css";
 
 export const LoginForm = () => {
-
   // ** Trae las funcionalidades del UseForm
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   // ** Navegacion
   const navigate = useNavigate();
@@ -19,28 +15,37 @@ export const LoginForm = () => {
   // ** Metodo Carga los Usuarios
   const loadUsers = async () => {
     const res = await getAllUser();
-    console.log(res.data);
     return res.data; // Lista de usuarios para validaciones
   };
 
   // ** Metodo de inicio de sesión
   const onSubmit = async (data) => {
+    // ** Validar campos vacios
+    if (!data.emailUsername && !data.password) {
+      toast.error("Por favor, completa todos los campos");
+      return;
+    } else if (!data.emailUsername) {
+      toast.error("Por favor, Ingresa tu correo o nombre de usuario");
+      return;
+    } else if (!data.password) {
+      toast.error("Por favor, Ingresa tu contraseña");
+      return;
+    }
+
     // Carga la lista de usuarios
     const users = await loadUsers();
 
     // Verificar si el usuario existe
     const userExists = users.some(
       (user) =>
-        (user.user_name === data.emailUsername ||
-          user.email === data.emailUsername) &&
+        (user.user_name === data.emailUsername || user.email === data.emailUsername) &&
         user.password === data.password
     );
 
     if (userExists) {
-      console.log("Usuario válido. Iniciando sesión...");
       navigate("/inicio");
     } else {
-      console.log("Usuario inválido. Verifique sus datos");
+      toast.error("Error de inicio de sesión. Verifica tus credenciales");
     }
   };
 
@@ -54,9 +59,8 @@ export const LoginForm = () => {
             type="text"
             id="emailUsername"
             placeholder="Email o Nombre de Usuario"
-            {...register("emailUsername", { required: true })}
+            {...register("emailUsername")}
           />
-          {errors.emailUsername && console.log("Login Campo Requerido")}
         </div>
 
         <div className="form__input-box-login">
@@ -64,9 +68,8 @@ export const LoginForm = () => {
             type="password"
             id="password"
             placeholder="Contraseña"
-            {...register("password", { required: true })}
+            {...register("password")}
           />
-          {errors.password && console.log("Login Campo Requerido")}
         </div>
 
         <div className="form__remember">
