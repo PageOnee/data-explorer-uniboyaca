c0988616e8f10c5be50e33cd218a2d6855605aa2
Maicol Rojas <maidanlrojas@gmail.com>
2024-04-07 17:25:55 -0500

    Agrega y estabiliza la estructura de la pagina de Analisis Historico

---
+++ client/src/modules/data/pages/reports-historical/HistoricalReport.jsx
@@ -0,0 +1,27 @@
+/// Liberias de react
+import React from 'react'
+
+/// Componentes
+import { SideMenu } from "../../../../components/sidemenu/SideMenu";
+import { ReportHistorical } from '../../components/report-historical/ReportHistorical'
+
+
+export const HistoricalReport = () => {
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+
+                {/* Menu lateral */}
+                <div className="col-2">
+                    <SideMenu />
+                </div>
+
+                {/* Contenido */}
+                <div className='col-10'>
+                    <ReportHistorical />
+                </div>
+
+            </div>
+        </div>
+    )
+}
