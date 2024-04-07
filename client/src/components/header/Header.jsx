6f003a51971c47e2ff96392c76b05a4375771c19
Maicol Rojas <maidanlrojas@gmail.com>
2024-04-06 19:19:08 -0500

    Modifica y estabiliza la estructura del header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,19 +1,38 @@
+/// Iconos
 import { FaSortDown } from "react-icons/fa";
+
+/// Estilos
 import "./Header.css";
 
-// Todo : Componente cabecera 
-export const Header = ({ title }) => {
+
+// Componente : Cabecera principal
+export const Header = ({ titleModule, titleSection }) => {
+
+  // Convertir a mayusculas
+  const titleModuleUpper = titleModule.toUpperCase();
+  const titleSectionUpper = titleSection.toUpperCase();
+
   return (
-    <div className="header">
+    <div className="col-12 d-flex flex-row justify-content-between align-items-center header">
+
+      {/* Titulo de la pagina */}
       <div className="header__title-header">
-        <h2>{title}</h2>
+        <h2>
+          <span className=""> {titleModuleUpper} </span>
+          <span className="mx-2"> / </span>
+          <span className="mx-2 text-red"> {titleSectionUpper} </span>
+        </h2>
       </div>
 
-      <div className="header__user-container">
-        <p>Maicol Rojas</p>
+      {/* Contendor - Informacion usuario */}
+      <div className="d-flex flex-row align-items-center header__user-container">
+
+        <p className="my-auto">Maicol Rojas</p>
         <img src="/assets/images/predefined-user.jpg" alt="Foto de Perfil del Usuario" />
         <FaSortDown />
+
       </div>
+
     </div>
   );
 };
