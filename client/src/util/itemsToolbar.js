8ea041e066cbffbb240c64d04afe92b3dba87553
Maicol Rojas <maidanlrojas@gmail.com>
2024-04-07 19:52:01 -0500

    Agrega los items de la barra de herramientas

---
+++ client/src/util/itemsToolbar.js
@@ -0,0 +1,84 @@
+export const levelItems = [
+  {
+    name: "Pregrado",
+  },
+  {
+    name: "Posgrado",
+  },
+];
+
+
+export const periodItems = [
+  {
+    name: "Semestral",
+  },
+  {
+    name: "Anual",
+  },
+  {
+    name: "General",
+  },
+];
+
+
+export const semesterLapseItems = [
+  {
+    name: "2020-2",
+  },
+  {
+    name: "2021-1",
+  },
+  {
+    name: "2021-2",
+  },
+  {
+    name: "2022-1",
+  },
+  {
+    name: "2022-2",
+  },
+  {
+    name: "2023-1",
+  },
+];
+
+
+export const annualLapseItems = [
+  {
+    name: "2020",
+  },
+  {
+    name: "2021",
+  },
+  {
+    name: "2022",
+  },
+  {
+    name: "2023",
+  },
+];
+
+
+export const categoryItems = [
+  {
+    name: "Informacion Personal",
+  },
+  {
+    name: "Informacion Familiar"
+  },
+  {
+    name: "Estado SocioEconomico"
+  },
+  {
+    name: "Educacion Previa",
+  },
+  {
+    name: "Contexto Social y Personal"
+  },
+  {
+    name: "Estilo de Vida"
+  },
+  {
+    name: "Percepcion y Satisfaccion",
+  }
+];
