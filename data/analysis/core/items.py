42de24fdcab4c211d0ac156a51d9432f654f49b2
Maicol Rojas <maidanlrojas@gmail.com>
2024-04-07 20:11:52 -0500

    Agrega los paths de los recursos y los items de los dataframes

---
+++ data/analysis/core/items.py
@@ -0,0 +1,37 @@
+
+class Items:
+    
+    ## Items Pregrado
+    
+    # Items categoria informacion personal
+    informacion_personal =  [
+                        'Sede','ProgramaAcademico', 'DoblePrograma', 'SedeDoblePrograma','ProgramaDoblePrograma','Sexo','Edad','EstadoCivil','NumeroHijos','ConviveCon',
+                        'CantidadPersonasCargoEconomicamente','EstratoSocioEconomico'
+                        ]
+        
+    # Items categoria percepcion
+    percepcion = [
+                'PercepcionEquiposMediosTecnlogicos', 'PercepcionCalidadDocentes', 'PercepcionPlanesEstudio','PercepcionServicioUsuario',
+                'PercepcionProcesosPedagogicos','PercepcionSistemaEvaluacion','PercepcionImagenGeneral','SatisfaccionChatLinea','SatisfaccionFormularioPaginaWeb',
+                'SatisfaccionWhatsapp','SatisfaccionMessenger','SatisfaccionCorreoElectronico','SatisfaccionPuntoAtenciónPresencial'
+                ]
+        
+    # Items categoria educacion previa
+    educacion_previa = ['JornadaBachillerato', 'ValidacionBachillerato', 'InstitucionBachilleratoBilingue','InconclusionEstudiosPrevios','NivelIdiomaIngles', 'NivelIdiomaFrances',
+                        'NivelIdiomaItaliano', 'NivelIdiomaAleman']
+    
+    
+    ## Items Posgrado
+    
+    # Items categoria informacion personal
+    informacion_personal_post = ['ProgramaAcademico', 'AlternativaDeGrado', 'Sede','Genero','Edad','EstadoCivil', 'CantidadHijos']
+        
+    # Items categoria percepcion
+    percepcion_post = ['PercepcionEquiposTecnlogicos','PercepcionInstalaciones', 'PercepcionCalidadDocentes', 'PercepcionPlanesEstudio','PercepcionServicioAlUsuario',
+                           'PercepcionProcesosPedagogicos','PercepcionGeneralUniversidad']
+    
+    
+    ### Items Historico
+    
+    # Items Predeterminados
+    historico_prueba = ['Sede','Sexo','EstadoCivil','PercepcionCalidadDocentes',]
