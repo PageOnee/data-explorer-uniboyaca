51740d724f4c984f139dc3705cfb0be7872b2a1d
Maicol Rojas <maidanlrojas@gmail.com>
2024-04-24 10:31:24 -0500

    update urls

--- client/src/services/data.api.js
+++ client/src/services/data.api.js
@@ -9,7 +9,7 @@ const URL =
 
 // Servicio : Traer datos - semestrales
 export const getInfoSemester = (level, lapsed, category, item, item_data) => {
-  const baseUrl = `${URL}report_period`;
+  const baseUrl = `${URL}/report_period`;
   const interval = 'Semestral';
 
   const url = `${baseUrl}?level=${level}&interval=${interval}&lapse=${lapsed}&category=${category}&item=${item}&item_data=${item_data}`;
@@ -20,7 +20,7 @@ export const getInfoSemester = (level, lapsed, category, item, item_data) => {
 
 // Servicio : Traer datos - anuales
 export const getInfoAnnual = (level, lapsed, category, item, item_data) => {
-  const baseUrl = `${URL}report_period`;
+  const baseUrl = `${URL}/report_period`;
   const interval = 'Anual';
 
   const url = `${baseUrl}?level=${level}&interval=${interval}&lapse=${lapsed}&category=${category}&item=${item}&item_data=${item_data}`;
@@ -30,7 +30,7 @@ export const getInfoAnnual = (level, lapsed, category, item, item_data) => {
 
 // Servicio : Traer datos - anuales
 export const getInfoPost = (level, interval, lapsed, category, item, item_data) => {
-  const baseUrl = `${URL}report_period`;
+  const baseUrl = `${URL}/report_period`;
 
 
   const url = `${baseUrl}?level=${level}&interval=${interval}&lapse=${lapsed}&category=${category}&item=${item}&item_data=${item_data}`;
@@ -42,7 +42,7 @@ export const getInfoPost = (level, interval, lapsed, category, item, item_data) 
 
 // Servicio : Traer datos - generales
 export const getInfo = (level, lapsed, category, item, item_data) => {
-  const baseUrl = `${URL}report_period`;
+  const baseUrl = `${URL}/report_period`;
   const interval = 'General';
 
   const url = `${baseUrl}?level=${level}&interval=${interval}&lapse=${lapsed}&category=${category}&item=${item}&item_data=${item_data}`;
@@ -53,7 +53,7 @@ export const getInfo = (level, lapsed, category, item, item_data) => {
 
 // Servicio : Traer datos - generales
 export const getHistorical = () => {
-  const baseUrl = `${URL}report_historical`;
+  const baseUrl = `${URL}/report_historical`;
 
   // const url = 'http://127.0.0.1:8000/data-explorer/api/v1/data/report_historical';
 
@@ -64,7 +64,7 @@ export const getHistorical = () => {
 // Servicio : Traer datos - generales
 export const getItems = (item) => {
   // const baseUrl = 'http://127.0.0.1:8000/data-explorer/api/v1/data/items-filter';
-  const baseUrl = `${URL}items-filter`;
+  const baseUrl = `${URL}/items-filter`;
 
   const url = `${baseUrl}?item=${item}`;
 
