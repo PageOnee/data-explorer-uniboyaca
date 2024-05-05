34e6eeb3d3e41be189df7064595eddefd8a741bc
Maicol Rojas <maidanlrojas@gmail.com>
2024-05-05 15:34:39 -0500

    Update Url API Autenticacion

--- client/src/services/users.api.js
+++ client/src/services/users.api.js
@@ -1,9 +1,16 @@
 /// Librerias de react
 import axios from "axios";
 
+// Url 
+const URL =
+  import.meta.env.MODE === "production"
+    ? import.meta.env.VITE_BACKEND_URL
+    : 'http://127.0.0.1:8000';
+
+
 // Url base Api - Usuarios
 const userApi = axios.create({
-  baseURL: "http://localhost:8000/data-explorer/api/v1/users/",
+  baseURL: `${URL}/data-explorer/api/v1/users/`
 });
 
 
